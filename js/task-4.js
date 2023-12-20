cedc3fead865d103cadd7a8b153784fa94d658b2
YevheniiaT <jane1talala@gmail.com>
2023-12-20 22:41:58 +0200

    add task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+// Обробка відправлення форми form.login-form повинна відбуватися за подією submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо при сабміті у формі є незаповнені поля, виводь alert з попередженням про те, що 'All form fields must be filled in'. Не додавай на інпути атрибут required, валідація має відбуватися саме через JS.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт з двома властивостями, де ключ — це ім'я інпутів, а значення — відповідні значення цих інпутів, очищені від пробілів по краях. Для доступу до елементів форми використовуй властивість elements.
+// При сабміті форми виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
+const form = document.querySelector('.login-form');
+const btn = document.querySelector('button');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+  const data = {
+    email: e.target.email.value,
+    password: e.target.password.value,
+  };
+    
+  console.log(data);
+  if (e.target.email.value === '' || e.target.password.value === '') {
+    alert('All form fields must be filled in');
+  }
+  form.reset();
+});
