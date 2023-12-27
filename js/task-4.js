27a8f4044c1c005c3933d8f8193d8f125c0669ac
YevheniiaT <jane1talala@gmail.com>
2023-12-27 22:06:12 +0200

    change task-4

--- js/task-4.js
+++ js/task-4.js
@@ -5,19 +5,16 @@
 // При сабміті форми виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
 const form = document.querySelector('.login-form');
-const btn = document.querySelector('button');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    
+form.addEventListener('submit', event => {
+  event.preventDefault();
   const data = {
-    email: e.target.email.value,
-    password: e.target.password.value,
+    email: event.target.elements.email.value.trim(),
+    password: event.target.elements.password.value.trim(),
   };
-    
-  console.log(data);
-  if (e.target.email.value === '' || e.target.password.value === '') {
-    alert('All form fields must be filled in');
+  if (data.email === '' || data.password == '') {
+    return alert('All form fields must be filled in');
   }
-  form.reset();
+  console.log(data);
+  event.target.reset();
 });
